76a1a16cde53ac384354aaa94ec9a7411e9eb772
Benson Li <bl4074@hotmail.com>
2015-12-25 16:52:44 -0800

    Finishing nva

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,22 +1,10 @@
 $(document).ready(function(){
     $("#intro, #arrow_down_box").click(function(){
-        $("#arrow_down").fadeOut("fast", function(){
-            if($("#hook_alignment").css("margin-top") == "400px") {
-                $("#hook_alignment").animate({"margin-top": "75px"}, 1000).css("cursor", "auto");
-                $("#arrow_down_box").css({"cursor" : "auto", "color" : "#c71f31"});
-                $("#about, #projects").fadeIn("slow");
-            }            
-        });
+        navIntro();
     });
     
     $("html, body").bind("DOMMouseScroll MouseScrollEvent MozMousePixelScroll wheel scroll", function(){
-        $("#arrow_down").fadeOut("fast", function(){
-            if($("#hook_alignment").css("margin-top") == "400px") {
-                $("#hook_alignment").animate({"margin-top": "75px"}, 1000).css("cursor", "auto");
-                $("#arrow_down_box").css({"cursor" : "auto", "color" : "#c71f31"});
-                $("#about, #projects").fadeIn("slow");
-            }            
-        });      
+        navIntro();
     });
     
     if($(".mobile_message").css("display") == "none") {
@@ -31,5 +19,18 @@ $(document).ready(function(){
             element.animate({marginTop: '-='+distance}, speed)
             .animate({marginTop: '+='+distance}, speed);
         }       
+    }
+    
+    // Nav Intro
+    function navIntro(){
+        $("#arrow_down_box").fadeOut("fast", function(){
+            if($("#hook_alignment").css("margin-top") == "400px") {
+                $("#hook").fadeOut("fast", function(){
+                    $("#hook_alignment").animate({"margin-top": "0", "width" : "100%", "height" : "50px"}, 1000).css({"cursor": "auto", "background-image" : "none", "box-shadow" : "0px 1px 1px 1px #999"});
+                    // $("#arrow_down_box").css({"cursor" : "auto", "color" : "#c71f31"});
+                    $("#about, #projects, ul").fadeIn("slow");                    
+                });          
+            }            
+        });        
     }
 });
