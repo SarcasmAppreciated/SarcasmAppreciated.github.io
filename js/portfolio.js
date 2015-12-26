bca92b9a69f9ff8aba912ab9ee4a4cab5f37e2b5
Benson Li <bl4074@hotmail.com>
2015-12-25 21:48:07 -0800

    Fix animation order

--- js/portfolio.js
+++ js/portfolio.js
@@ -26,9 +26,11 @@ $(document).ready(function(){
         $("#arrow_down_box").fadeOut("fast", function(){
             if($("#hook_alignment").css("margin-top") == "400px") {
                 $("#hook").fadeOut("fast", function(){
-                    $("#hook_alignment").animate({"margin-top": "0", "width" : "100%", "height" : "50px"}, 1000).css({"cursor": "auto", "background-image" : "none", "box-shadow" : "0px 1px 1px 1px #999"});
-                    // $("#arrow_down_box").css({"cursor" : "auto", "color" : "#c71f31"});
-                    $("#about, #projects, ul").fadeIn("slow");                    
+                    $("#hook_alignment").animate({"margin-top": "0", "width" : "100%", "height" : "50px"}, 1000, function(){
+                        $(this).css({"cursor": "auto", "background-image" : "none", "box-shadow" : "0px 1px 1px 1px #999"});
+                        $("#about, #projects, ul").fadeIn("slow");  
+                    });
+                    // $("#arrow_down_box").css({"cursor" : "auto", "color" : "#c71f31"});                  
                 });          
             }            
         });        
