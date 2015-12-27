5b3035e68a8f5ecb717145d0ac0dc1ec573b8c9a
Benson Li <bl4074@hotmail.com>
2015-12-27 11:56:02 -0800

    Azimuth complete

--- js/page.js
+++ js/page.js
@@ -5,9 +5,11 @@ $(document).ready(function(){
     function navIntro(){
         $("#hook").delay(1000).fadeOut("fast", function(){
             $("#hook_alignment").animate({"margin-top": "0", "width" : "100%", "height" : "50px"}, 1000, function(){
-                $(this).css({"cursor": "auto", "background-image" : "none", "box-shadow" : "0px 1px 1px 1px #999"});
-                $(".azimuth, ul").fadeIn("slow");  
+                $(this).css({"cursor": "auto", "background" : "#FFF", "box-shadow" : "0px 1px 1px 1px #999", "position" : "fixed", "left" : "0"});
+                $(".azimuth, #projects, ul").fadeIn("slow");  
             });                
         });        
     }
+    
+    
 });
