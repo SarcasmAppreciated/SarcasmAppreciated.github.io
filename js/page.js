df17768a053a739e47f044c6650a4ae6669fdaf4
Benson Li <bl4074@hotmail.com>
2015-12-27 12:32:20 -0800

    Azimuth mobile fixes

--- js/page.js
+++ js/page.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
         $("#hook").delay(1000).fadeOut("fast", function(){
             $("#hook_alignment").animate({"margin-top": "0", "width" : "100%", "height" : "50px"}, 1000, function(){
                 $(this).css({"cursor": "auto", "background" : "#FFF", "box-shadow" : "0px 1px 1px 1px #999", "position" : "fixed", "left" : "0"});
-                $(".azimuth, #projects, ul").fadeIn("slow");  
+                $(".azimuth, #project, ul").fadeIn("slow");  
             });                
         });        
     }
