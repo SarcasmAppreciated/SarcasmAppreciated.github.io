c7b3db4db623e8fe69d5898fe1dd07e3f4cb5f25
Benson Li <bl4074@hotmail.com>
2015-12-26 20:46:31 -0800

    Mobile fixes

--- js/page.js
+++ js/page.js
@@ -1,21 +1,13 @@
 $(document).ready(function(){
-    $("#intro").click(function(){
-        navIntro();
-    });
-    
-    $("html, body").bind("DOMMouseScroll MouseScrollEvent MozMousePixelScroll wheel scroll", function(){
-        navIntro();
-    });
+    navIntro();
     
     // Nav Intro
     function navIntro(){
-        if($("#hook_alignment").css("margin-top") == "400px") {
-            $("#hook").fadeOut("fast", function(){
-                $("#hook_alignment").animate({"margin-top": "0", "width" : "100%", "height" : "50px"}, 1000, function(){
-                    $(this).css({"cursor": "auto", "background-image" : "none", "box-shadow" : "0px 1px 1px 1px #999"});
-                    $(".azimuth, ul").fadeIn("slow");  
-                });                
-            });          
-        }       
+        $("#hook").delay(1000).fadeOut("fast", function(){
+            $("#hook_alignment").animate({"margin-top": "0", "width" : "100%", "height" : "50px"}, 1000, function(){
+                $(this).css({"cursor": "auto", "background-image" : "none", "box-shadow" : "0px 1px 1px 1px #999"});
+                $(".azimuth, ul").fadeIn("slow");  
+            });                
+        });        
     }
 });
