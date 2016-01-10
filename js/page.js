c9d29f1f396bb0584bc14bb875146fa133e1568a
Benson Li <bl4074@hotmail.com>
2016-01-10 13:37:43 -0800

    Added HHHPolls

--- js/page.js
+++ js/page.js
@@ -1,15 +1,14 @@
 $(document).ready(function(){
     navIntro();
-    
+
     // Nav Intro
     function navIntro(){
+
         $("#hook").delay(1000).fadeOut("fast", function(){
             $("#hook_alignment").animate({"margin-top": "0", "width" : "100%", "height" : "50px"}, 1000, function(){
-                $(this).css({"cursor": "auto", "background" : "#FFF", "box-shadow" : "0px 1px 1px 1px #999", "position" : "fixed", "left" : "0"});
-                $(".azimuth, #project, ul").fadeIn("slow");  
+                $(this).css({"cursor": "auto", "background" : "#FFF", "box-shadow" : "0px 1px 1px 1px #999", "position" : "fixed", "z-index" : "100", "left" : "0"});
+                $(".azimuth, #project, ul").fadeIn("slow");
             });                
-        });        
-    }
-    
-    
+        });    
+    } 
 });
