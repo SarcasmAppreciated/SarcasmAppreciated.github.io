9b268bc915321cae56604ce3688600eb0ab3e091
Benson Li <bl4074@hotmail.com>
2016-03-26 14:07:18 -0700

    FFT Testing: Spectrum

--- js/sketch.js
+++ js/sketch.js
@@ -1,10 +1,10 @@
 var sound, fft;
 
 function preload() {
-    sound = loadSound('../audio/v-09-11-3-8.wav', triggerFFT);    
+    sound = loadSound('../audio/v-09-11-3-8.wav');    
 }
 
-function triggerFFT(){
+function draw(){
   console.log("Loaded!");
   fft = new p5.FFT();
   
