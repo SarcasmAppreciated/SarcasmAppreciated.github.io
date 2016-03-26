346cac6b777cad710ea22872ccd9156a7fc2a110
Benson Li <bl4074@hotmail.com>
2016-03-26 14:04:52 -0700

    FFT Testing: Spectrum

--- js/sketch.js
+++ js/sketch.js
@@ -1,49 +1,13 @@
 var sound, fft;
 
 function preload() {
-    sound = loadSound('../audio/v-09-11-3-8.wav');
-    
+    sound = loadSound('../audio/v-09-11-3-8.wav', triggerFFT);    
 }
 
-function setup(){
-  var cnv = createCanvas(100,100);
-  cnv.mouseClicked(togglePlay);
+function triggerFFT(){
+  console.log("Loaded!");
   fft = new p5.FFT();
-  sound.amp(0.2);
-}
-
-function draw(){
-  background(0);
-
-  var spectrum = fft.analyze(); 
-  noStroke();
-  fill(0,255,0); // spectrum is green
-  for (var i = 0; i< spectrum.length; i++){
-    var x = map(i, 0, spectrum.length, 0, width);
-    var h = -height + map(spectrum[i], 0, 255, height, 0);
-    rect(x, height, width / spectrum.length, h )
-  }
-
-  var waveform = fft.waveform();
-  noFill();
-  beginShape();
-  stroke(255,0,0); // waveform is red
-  strokeWeight(1);
-  for (var i = 0; i< waveform.length; i++){
-    var x = map(i, 0, waveform.length, 0, width);
-    var y = map( waveform[i], -1, 1, 0, height);
-    vertex(x,y);
-  }
-  endShape();
-
-  text('click to play/pause', 4, 10);
-}
-
-// fade sound if mouse is over canvas
-function togglePlay() {
-  if (sound.isPlaying()) {
-    sound.pause();
-  } else {
-    sound.loop();
-  }
+  
+  var spectrum = fft.analyze();
+  console.log(spectrum);
 }
