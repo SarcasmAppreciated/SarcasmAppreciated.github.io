ffd2c5b9417c8493822a0fe704590e4eeea2815a
Benson Li <bl4074@hotmail.com>
2016-03-27 15:13:27 -0700

    File Reading

---
+++ js/main.js
@@ -0,0 +1,57 @@
+$(document).ready(function(){
+    var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+    
+    var audio = document.getElementById("myAudio");
+    var source = audioCtx.createMediaElementSource(audio);    
+    var analyser = audioCtx.createAnalyser();
+    
+    source.connect(analyser);
+    source.connect(audioCtx.destination);
+    var frequencyData = new Float32Array(analyser.frequencyBinCount);
+    var timeData = frequencyData;
+    
+    function renderFrame() {
+        analyser.getFloatFrequencyData(frequencyData);
+        $("#frequency").val(frequencyData);
+        
+        analyser.getFloatTimeDomainData(timeData);
+        $("#time").val(timeData);
+    }
+    
+    var isPlaying = true;        
+    audio.onplay = function() {        
+        setInterval(function(){
+            if (isPlaying)
+                renderFrame();                
+        }, 1);               
+    };
+    
+    audio.addEventListener('ended', changeState, false);
+    
+    function changeState(){
+        isPlaying = false;        
+    }
+    
+    // analyzer.fftSize = 2048;
+    // var bufferLength = analyzer.frequencyBinCount;
+    // var dataArray = new Uint8Array(bufferLegnth);
+    // anaylzer.getByteTimeDomainData(dataArray);
+   
+    /*
+    var load = function(event) {
+        source = audioCtx.createBufferSource();
+        var input = event.target;       
+        var reader = new FileReader();
+        reader.onload = function(){
+            var arrayBuffer = reader.result;
+            audioContext.decodeAudioData(arrayBuffer, function(buffer) {
+                source.buffer = buffer;
+                // source.connect(audioCtx.destination);
+                
+            });
+        }
+        reader.readAsArrayBuffer(input.files[0]);
+    }
+    */
+    
+});
