20e14ab95ee14c789e5f160c1e068b146568bbc9
Benson Li <bl4074@hotmail.com>
2016-03-27 18:33:31 -0700

    Frequency Visualization

--- js/main.js
+++ js/main.js
@@ -1,21 +1,26 @@
 $(document).ready(function(){
-    var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-    
+    var audioCtx = new (window.AudioContext || window.webkitAudioContext)();    
     var audio = document.getElementById("myAudio");
     var source = audioCtx.createMediaElementSource(audio);    
     var analyser = audioCtx.createAnalyser();
     
+    analyser.smoothingTimeConstant = 0;
+    
     source.connect(analyser);
     source.connect(audioCtx.destination);
-    var frequencyData = new Float32Array(analyser.frequencyBinCount);
+    var bufferLength = analyser.frequencyBinCount;
+    var frequencyData = new Uint8Array(bufferLength);
     var timeData = frequencyData;
     
+    
+    var x = 0;
     function renderFrame() {
-        analyser.getFloatFrequencyData(frequencyData);
+        analyser.getByteFrequencyData(frequencyData);
         $("#frequency").val(frequencyData);
         console.log(frequencyData);
+        draw(frequencyData);
         
-        analyser.getFloatTimeDomainData(timeData);
+        analyser.getByteTimeDomainData(timeData);
         $("#time").val(timeData);
     }
     
@@ -34,11 +39,40 @@ $(document).ready(function(){
         isPlaying = false;        
     }
     
-    // analyzer.fftSize = 2048;
-    // var bufferLength = analyzer.frequencyBinCount;
-    // var dataArray = new Uint8Array(bufferLegnth);
-    // anaylzer.getByteTimeDomainData(dataArray);
-   
+    
+    var canvas = document.getElementById('visualizer');
+    var canvasCtx = canvas.getContext("2d");
+    canvas.setAttribute('width', 800);
+    canvas.setAttribute('height', 200);
+    
+    var drawVisual;
+    var WIDTH = canvas.width;
+    var HEIGHT = canvas.height;
+    canvasCtx.fillStyle = 'rgb(0, 0, 0)';
+    canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
+    
+            
+    function draw(dataArray) {
+        canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
+        canvasCtx.fillStyle = 'rgb(0, 0, 0)';
+        canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
+        
+        var barWidth = (WIDTH / bufferLength) * 2;
+        var barHeight;
+        
+        var x = 0;
+        for(var i = 0; i < bufferLength; i++) {
+            barHeight = -1 * dataArray[i];
+        
+            canvasCtx.fillStyle = 'rgb(' + (barHeight+100) + ',50,50)';
+            canvasCtx.fillRect(x, HEIGHT-barHeight/2, barWidth, barHeight/2);
+
+            x += barWidth + 1;
+        }
+    };
+
+    
+    
     /*
     var load = function(event) {
         source = audioCtx.createBufferSource();
