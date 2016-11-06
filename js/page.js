9cefc79c8c2b9590b9fbb797a56572f993fecea8
Benson Li <bl4074@hotmail.com>
2016-11-05 18:06:24 -0700

    Being Update

--- js/page.js
+++ js/page.js
@@ -4,11 +4,11 @@ $(document).ready(function(){
     // Nav Intro
     function navIntro(){
 
-        $("#hook").delay(1000).fadeOut("fast", function(){
+        // $("#hook").delay(1000).fadeOut("fast", function(){
             $("#hook_alignment").animate({"margin-top": "0", "width" : "100%", "height" : "50px"}, 1000, function(){
                 $(this).css({"cursor": "auto", "background" : "#FFF", "box-shadow" : "0px 1px 1px 1px #999", "position" : "fixed", "z-index" : "100", "left" : "0"});
                 $(".azimuth, #project, ul").fadeIn("slow");
             });                
-        });    
+        // });    
     } 
 });
--- js/portfolio.js
+++ js/portfolio.js
@@ -4,14 +4,22 @@ $(document).ready(function(){
 
     // Nav Intro
     function navIntro(){
-        $("#hook").delay(1000).fadeOut("fast", function(){
-            $("#hook_alignment").animate({"margin-top": "0", "width" : "100%", "height" : "50px"}, 1000, function(){
-                $(this).css({"cursor": "auto", "background" : "#FFF", "box-shadow" : "0px 1px 1px 1px #999", "position" : "fixed", "z-index" : "100", "left" : "0"});
-                $("#about, ul, .azimuth, #project").fadeIn("slow");
-            });                
-        });    
-    } 
+        // $("#hook").delay(1000).fadeOut("fast", function(){
+            // $("#hook_alignment").animate({"margin-top": "0", "width" : "100%", "height" : "50px"}, 1000, function(){
+                // $(this).css({"cursor": "auto", "background" : "#FFF", "box-shadow" : "0px 1px 1px 1px #999", "position" : "fixed", "z-index" : "100", "left" : "0"});
+                // $(this).css({});
+                $("#about, ul, .azimuth, #project, #splash").fadeIn("slow");
+            // });                
+        // });    
+    }
     
+    $("#arrow_down").click(function(){
+        var target = $("#about");
+        $('html, body').animate({
+            scrollTop: target.offset().top
+        }, 1000);        
+    });
+
     /*
     $("#intro, #arrow_down_box").click(function(){
         navIntro();
