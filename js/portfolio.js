7c758968ac8bf402551add7c0f78b5a0f7f39222
Benson Li <bl4074@hotmail.com>
2017-03-01 19:46:48 -0800

    Navigation

--- js/portfolio.js
+++ js/portfolio.js
@@ -13,9 +13,28 @@ $(document).ready(function(){
         $("#main").fullpage({
             bigSectionsDestination: "top",
             css3: true,
+            afterLoad: function(anchor, index) {
+                var $currentNav = $("ul#navigation").find("[data='" + index +"']").children("li");
+                $currentNav.addClass("active strikethrough");
+                $("ul#navigation").find("li").not($currentNav).removeClass("active strikethrough");
+            },
             responsiveHeight: 940
         });
     }
+       
+    $( window ).scroll(function() {
+        if($(".fp-responsive").length > 0) {
+            if($( window ).scrollTop() != 0) {
+                $("h1").css("display", "none");
+                $("ul#social_media").css("margin-top", "5px");
+                $("ul#navigation").css("margin-top", "25px");
+            } else {
+                $("h1").css("display", "block");
+                $("ul#social_media").css("margin-top", "calc(2% + 35px)");
+                $("ul#navigation").css("margin-top", "calc(2% + 55px)");
+            }
+        }
+    });
     
     /* Parallax */
     
