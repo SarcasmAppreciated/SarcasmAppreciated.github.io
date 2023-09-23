e3feb5fb8fc02184bd0e3f98f31b502b84a4f86f
Benson Li <bl4074@hotmail.com>
2023-09-22 23:47:20 -0700

    Updates

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,41 +1,78 @@
 $(document).ready(function(){
    
     function checkMobile() {
-        var returnVal = 0;
         if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-            returnVal = 1000;
+            return true;
         }
-        return returnVal;
+        return false;
     }
 
 	const copyright = "Benson Li 2015 - " + new Date().getFullYear() + " ©";
 	$("#copyright").text(copyright);
 
-    /* HHHP */
-    $("#start_hhhp").click(function() {
-        $(".hhhp_hide, #social_media, h1").fadeOut( function() {
-            window.location.href = "hhhp.html";
-        });        
-    });
-    
-    const gridElements = [
-        {background: "intro", title: "", text: "", size: "four", clickThrough: ""},
-        {background: "", title: "Hi, I like making cool things - beautiful and immersive experiences.", text: "", size: "four", clickThrough: "true"},
+    const skillElements = [
+        {size: "80", text: "// Java"},
+        {size: "40", text: "// C#"},
+        {size: "60", text: "// Android"},
+        {size: "100", text: "&#60; HTML &#62;"},
+        {size: "100", text: "{ CSS }"},
+        {size: "90", text: ".javascript"},
+        {size: "70", text: "react()"},
+        {size: "80", text: "$.jQuery"},
+        {size: "60", text: "-- LUA"},
+        {size: "60", text: "UI/UX"}
     ];
-    populateGrid();
-    function renderElement(element) {
-        $(".stage").append(`<div id="${element.background}-bg" class="stage_item ${element.size}" onClick="expandElement()">
-            <div style="margin:10px">
-                <h2>${element.title}</h2>
-                <p class="stage_subtext">${element.text}</p>
-            </div>
-        </div>`);
-    }
-    function populateGrid() {
-        gridElements.forEach((element) => renderElement(element));
-    }
+    const gridElements = [
+        {background: "hll", title: "Hotline League and Travis Gafford", text: "- Circa. 2018 to Present -", clickThrough: "https://www.behance.net/gallery/56894655/Overlays-and-Branding", clickText: "Behance Page"},
+        {background: "amazon", title: "Amazon: Software Engineer", text: "- June 2017 to June 2023 -<br>Amazon Display Ad Products<br>Architected and developed products<br>generating millions of revenue", clickThrough: "https://amazonfiretv.blog/immerse-yourself-in-middle-earth-with-the-lord-of-the-rings-the-rings-of-power-bb76cc29a9ff", clickText: "Case Study"},
+        {background: "poki", title: "Pokimane Podcast", text: "- Completed March 2018 -<br>Dynamic start and intermission screens", clickThrough: "https://archive.org/details/twitch-vod-v282019111", clickText: "Podcast"},
+        {background: "ample", title: "Ample Food Icons", text: "- Completed January 2017 -<br>Icons and Infographic", clickThrough: "https://www.behance.net/gallery/49921415/AmpleMealcom", clickText: "Behance Page"},
+        {background: "hhhp", title: "HipHopHeads Polls", text: "- Circa March 2016 -<br>Polled Music Player", clickThrough: "", clickText: "Case Study"},
+        {background: "gamespot", title: "Gamespot", text: "- March 2014 - February 2016 -<br>Graphics design for onGamers and G | League", clickThrough: "https://www.behance.net/SarcasmAppreciated", clickText: "Behance Page"},
+        {background: "fiist", title: "Fiist: Waitlist Management", text: "- Circa. 2012 -<br>Project page for Fiist (startup); designed by Brian Leung", clickThrough: "http://sarcasmappreciated.github.io/Fiist-Site/", clickText: "Github Page"}
+    ];
+    const people = [
+        {clickThrough: "https://twitter.com/pokimanelol", name: "pokimane"},
+        {clickThrough: "https://ateyo.com/", name: "ateyo"},
+        {clickThrough: "https://www.amplemeal.com/", name: "ample"},
+        {clickThrough: "https://twitter.com/LGDgaming", name: "lgd"},
+        {clickThrough: "https://uwaterloo.ca/conrad-business-entrepreneurship-technology/", name: "uw"}
+    ];
 
-    function expandElement() {
-        
+    populateElements();
+
+    function renderSkills(element) {
+        $("#skills").append(`
+            <div class="skill" style="background-size: ${element.size}%">${element.text}</div>
+        `);
+    }
+    function renderPeople(element) {
+        $("#clients").append(`
+            <a href="${element.clickThrough}">
+                <div class="circle ${element.name}"></div>
+            </a>
+        `);
+    }
+    function renderGrid(element) {
+        $(".stage").append(`
+            <div class="stage-item" id="${element.background}">    
+                <div class="stage-item-image" id="${element.background}-bg" />
+                <div class="stage-item-text">
+                    <h2>${element.title}</h2>
+                    <p class="stage-subtext">${element.text}</p>
+                    ${element.clickThrough ? '<a href="' + element.clickThrough + '">' : '<div>'}<button class="stage-button">${element.clickText}</button></a>${element.clickThrough ? '</a>' : '</div>'}
+                </div>
+            </div>
+        `);
+    }
+    function populateElements() {
+        people.forEach((element) => renderPeople(element));
+        skillElements.forEach((element) => renderSkills(element));
+        gridElements.forEach((element) => renderGrid(element));
+        $("#hhhp").find("button").click(function() {
+            $("#main, #social_media, h1").fadeOut(function() {
+                window.location.href = "hhhp.html";
+            });
+        });
     }
 });
